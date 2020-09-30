6c73750325e81a68d0a222d4c7bad4722484c240
sash7410 <sashankrm11@gmail.com>
2020-10-01 04:46:55 +0530

    Update A_Next_Round.cpp

--- A_Next_Round.cpp
+++ A_Next_Round.cpp
@@ -1,14 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+#define gc getchar_unlocked
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef vector<int>		vi;
+typedef vector<pii>		vpii;
+typedef vector<vi>		vvi;
+//=======================
+const int MOD = 1'000'000'007;
+const int N = 2e6+13, M = N;
+//=======================
+vi g[N];
+int a[N];
+int n, m, k;
+//=======================
+
+void solve() {
     int n,k,c=0;
     cin>>n>>k;
     k--;
     int a[n];
-    for(int i=0;i<n;i++){
+    fo(i,n){
         cin>>a[i];
     }
-    for(int i=0;i<n;i++){
+    fo(i,n){
         if(a[i]>=a[k]&&a[i]>0){
             c++;
         }
@@ -16,4 +39,12 @@ int main(){
 
   
     cout<<c<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+      solve();
+    
+    return 0;
 }
