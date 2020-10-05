cebc7aed36c23806c9bbce3db68afafc873be2a8
sash7410 <sashankrm11@gmail.com>
2020-10-06 03:11:57 +0530

    Create B_Petya_and_Staircases.cpp

---
+++ B_Petya_and_Staircases.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef vector<int>		vi;
+typedef vector<pii>		vpii;
+typedef vector<vi>		vvi;
+//=======================
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+	uniform_int_distribution<int> uid(0,lim-1);
+	return uid(rang);
+}
+const int mod = 1000000007;
+const int N = 2e6+13, M = N;
+
+int mpow(int base, int exp); 
+//=======================
+
+//=======================
+
+bool solve() {
+    int m,n;
+   cin>>m>>n;
+  map<int,int> a;
+   bool f=0;
+   fo(i,n){
+        int x;
+        cin>>x;
+        if(x==m||x==1){
+            f=1;
+        }
+        a[x]++;
+    }
+   if(f)
+    return 0;
+    for(auto it=a.begin();it!=a.end();it++){
+        int x=it->first;
+        if((a.find(x+1)!=a.end())&&(a.find(x+2)!=a.end())){
+            return 0;
+        }
+    }
+    return 1;
+   
+}
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    if(solve()){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
+int mpow(int base, int exp) {
+	base %= mod;
+	int result = 1;
+	while (exp > 0) {
+	  if (exp & 1) result = ((ll)result * base) % mod;
+	  base = ((ll)base * base) % mod;
+	  exp >>= 1;
+	}
+	return result;
+  }
+
