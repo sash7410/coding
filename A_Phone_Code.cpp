2206407bcaf2bb533f2a8c1f48530dc53508f1c8
sash7410 <sashankrm11@gmail.com>
2020-10-09 23:26:10 +0530

    pb hustle 3.2

---
+++ A_Phone_Code.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef vector<int>		vi;
+typedef vector<pii>		vpii;
+typedef vector<vi>		vvi;
+//=======================
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+	uniform_int_distribution<int> uid(0,lim-1);
+	return uid(rang);
+}
+const int mod = 1000000007;
+
+int mpow(int base, int exp); 
+//=======================
+
+//=======================
+
+int solve() {
+    int n,c=0;
+    int m1=9999;
+    cin>>n;
+    vector<string > a;
+    bool f=0;
+    fo(i,n){
+        string x;
+  cin>>x;
+  a.push_back(x);
+  //m1=std::min(x.length(),m1);
+    }
+  for(int i=0;i<a[0].length();i++){
+      for(int j=1;j<n;j++){
+          if(a[j][i]!=a[0][i]){
+              f=1;
+              ///cout<<a[j][i]<<" "<<a[0][i]<<endl;
+              return i;
+          }
+      }
+      
+      
+          c++;
+      
+      
+  }
+    
+    return c;
+}
+    
+
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+  
+      cout<<solve()<<endl;
+    
+
+    return 0;
+}
+int mpow(int base, int exp) {
+	base %= mod;
+	int result = 1;
+	while (exp > 0) {
+	  if (exp & 1) result = ((ll)result * base) % mod;
+	  base = ((ll)base * base) % mod;
+	  exp >>= 1;
+	}
+	return result;
+  }
+
